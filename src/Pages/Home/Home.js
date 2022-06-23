83c994963466d81cfb1527f05af5c1330027fb4a
shakib hasan <shakibhasan@gmail.com>
2022-06-24 04:02:39 +0600

    counter section create

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faFacebook } from '@fortawesome/free-brands-svg-icons';
-import { faBookAtlas, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faBookAtlas, faCalendar, faCertificate, faPeopleLine, faPeopleRoof, faStar } from '@fortawesome/free-solid-svg-icons';
 
 import React from 'react';
 import { Container, Nav, Col, Row, Navbar, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
@@ -9,6 +9,36 @@ import './Home.css'
 import { faStrava } from '@fortawesome/free-brands-svg-icons';
 
 const Home = () => {
+    let counts = setInterval(updated);
+    let counts2 = setInterval(updated);
+    let counts3 = setInterval(updated);
+    let counts4 = setInterval(updated);
+    let upto = 0;
+    let upto2 = 0;
+    let upto3 = 0;
+    let upto4 = 10;
+    function updated() {
+        var count = document.getElementById("counter");
+        var count2 = document.getElementById("counter2");
+        var count3 = document.getElementById("counter3");
+        var count4 = document.getElementById("counter4");
+        count.innerHTML = ++upto;
+        if (upto === 1000) {
+            clearInterval(counts);
+        }
+        count2.innerHTML = ++upto2;
+        if (upto2 === 350) {
+            clearInterval(counts2);
+        }
+        count3.innerHTML = ++upto3;
+        if (upto3 === 570) {
+            clearInterval(counts3);
+        }
+        count4.innerHTML = ++upto4;
+        if (upto4 === 1024) {
+            clearInterval(counts4);
+        }
+    }
     return (
         <>
             <div className='home home-section'>
@@ -70,6 +100,7 @@ const Home = () => {
                 </div>
             </div>
             <div className='Expertise'>
+
                 <Container>
                     <Row>
                         <Col lg='6' md='6' sm='12'>
@@ -79,7 +110,8 @@ const Home = () => {
                             <p className='expertise-subheading'>LEARN ANYTHING</p>
                             <h2 className='expertise-heading'>Benefits About Online Learning Expertise</h2>
                             <div className='d-flex justify-content-center align-items-center exp-content'>
-                                <FontAwesomeIcon icon={faBookAtlas} className='exp-icons'/>
+
+                                <FontAwesomeIcon icon={faBookAtlas} className='counter-icons' />
                                 <div>
                                     <h3>Online Courses</h3>
                                     <p>Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
@@ -87,7 +119,7 @@ const Home = () => {
 
                             </div>
                             <div className='d-flex justify-content-center align-items-center exp-content'>
-                                <FontAwesomeIcon icon={faBookAtlas} className='exp-icons'/>
+                                <FontAwesomeIcon icon={faCertificate} className='counter-icons' />
                                 <div>
                                     <h3>Earn A Certificates</h3>
                                     <p>Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
@@ -95,13 +127,60 @@ const Home = () => {
 
                             </div>
                             <div className='d-flex justify-content-center align-items-center exp-content'>
-                                <FontAwesomeIcon icon={faBookAtlas} className='exp-icons'/>
+                                <FontAwesomeIcon icon={faPeopleLine} className='counter-icons' />
                                 <div>
                                     <h3>Learn with Expert</h3>
                                     <p>Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
                                 </div>
 
                             </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+
+            <div className='counter-section'>
+                <Container>
+                    <Row>
+                        <Col lg='3' md='6' sm='12'>
+                            <div className='d-flex justify-content-center align-items-center counter-content'>
+                                <FontAwesomeIcon icon={faPeopleLine} className='counter-icons' />
+                                <div>
+                                    <div id="counter"></div>
+                                    <p>Success Stories</p>
+                                </div>
+                            </div>
+
+                        </Col>
+                        <Col lg='3' md='6' sm='12'>
+                            <div className='d-flex justify-content-center align-items-center counter-content'>
+                                <FontAwesomeIcon icon={faPeopleRoof} className='counter-icons' />
+                                <div>
+                                    <div id="counter2"></div>
+                                    <p>Trusted Tutors</p>
+                                </div>
+                            </div>
+
+                        </Col>
+                        <Col lg='3' md='6' sm='12'>
+                            <div className='d-flex justify-content-center align-items-center counter-content'>
+                                <FontAwesomeIcon icon={faCalendar} className='counter-icons' />
+                                <div>
+                                    <div id="counter3"></div>
+                                    <p>Schedules</p>
+                                </div>
+                            </div>
+
+                        </Col>
+                        <Col lg='3' md='6' sm='12'>
+                            <div className='d-flex justify-content-center align-items-center counter-content'>
+                                <FontAwesomeIcon icon={faBookAtlas} className='counter-icons' />
+                                <div>
+                                    <div id="counter4"></div>
+                                    <p>Courses</p>
+                                </div>
+                            </div>
+
                         </Col>
                     </Row>
                 </Container>
