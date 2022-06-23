21f54685344ef62d47245c467b2f6e05b0fbdd1b
shakib hasan <shakibhasan@gmail.com>
2022-06-24 02:57:42 +0600

    expertise section create

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faFacebook } from '@fortawesome/free-brands-svg-icons';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faBookAtlas, faStar } from '@fortawesome/free-solid-svg-icons';
 
 import React from 'react';
 import { Container, Nav, Col, Row, Navbar, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
@@ -10,64 +10,103 @@ import { faStrava } from '@fortawesome/free-brands-svg-icons';
 
 const Home = () => {
     return (
-        <div className='home home-section'>
-            <div className='home-content'>
+        <>
+            <div className='home home-section'>
+                <div className='home-content'>
 
-                <Container >
-                    <Row>
-                        <Col lg='4' md='6' sm='12' className='left-content img-thumbnail'>
-                            <h4>Start your favourite course</h4>
-                            <h2>Now learning from anywhere, and build your <span>bright career.</span> </h2>
-                        </Col>
-                        <Col lg='8' md='6' sm='12' className='right-content'>
-                            <div className='course'>
-                                <p>1,024 Courses</p>
-                            </div>
-                            <div className='course2'>
-                                <p>4.8 <FontAwesomeIcon icon={faStar} className='ratings' /> Ratings</p>
-                           
-                            </div>
-                            <div class="curved-arrow vert-move">
+                    <Container >
+                        <Row>
+                            <Col lg='4' md='6' sm='12' className='left-content img-thumbnail'>
+                                <h4>Start your favourite course</h4>
+                                <h2>Now learning from anywhere, and build your <span>bright career.</span> </h2>
+                            </Col>
+                            <Col lg='8' md='6' sm='12' className='right-content'>
+                                <div className='course'>
+                                    <p>1,024 Courses</p>
+                                </div>
+                                <div className='course2'>
+                                    <p>4.8 <FontAwesomeIcon icon={faStar} className='ratings' /> Ratings</p>
 
-                            </div>
-                            <img src="https://i.ibb.co/LnG10bx/slider-img1.png" alt="" class="img-thumbnail" />
-                        </Col>
-                        <div class="box"></div>
-                        <div class="box"></div>
-                        <div class="box"></div>
-                        <div class="box"></div>
-                        <div class="box"></div>
-                        <div class="box"></div>
-                    </Row>
-                    <ul class="circles">
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </Container>
-                <div>
-                    <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
-                        <defs>
-                            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-                        </defs>
-                        <g class="parallax">
-                            <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(48,146,85,0.7" />
-                            <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(48,146,85,0.5)" />
-                            <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(48,146,85,85.3)" />
-                            <use xlinkHref="#gentle-wave" x="48" y="18" fill="#212832" />    
+                                </div>
+                                <div class="curved-arrow vert-move">
 
-                        </g>
-                    </svg>
+                                </div>
+                                <img src="https://i.ibb.co/LnG10bx/slider-img1.png" alt="" class="img-thumbnail" />
+                            </Col>
+                            <div class="box"></div>
+                            <div class="box"></div>
+                            <div class="box"></div>
+                            <div class="box"></div>
+                            <div class="box"></div>
+                            <div class="box"></div>
+                        </Row>
+                        <ul class="circles">
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </Container>
+                    <div>
+                        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+                            <defs>
+                                <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                            </defs>
+                            <g class="parallax">
+                                <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(48,146,85,0.7" />
+                                <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(48,146,85,0.5)" />
+                                <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(48,146,85,85.3)" />
+                                <use xlinkHref="#gentle-wave" x="48" y="18" fill="#0f1d2d" />
+
+                            </g>
+                        </svg>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className='Expertise'>
+                <Container>
+                    <Row>
+                        <Col lg='6' md='6' sm='12'>
+                            <img src="https://i.ibb.co/LnG10bx/slider-img1.png" alt="" class="img-thumbnail" />
+                        </Col>
+                        <Col lg='6' md='6' sm='12'>
+                            <p className='expertise-subheading'>LEARN ANYTHING</p>
+                            <h2 className='expertise-heading'>Benefits About Online Learning Expertise</h2>
+                            <div className='d-flex justify-content-center align-items-center exp-content'>
+                                <FontAwesomeIcon icon={faBookAtlas} className='exp-icons'/>
+                                <div>
+                                    <h3>Online Courses</h3>
+                                    <p>Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
+                                </div>
+
+                            </div>
+                            <div className='d-flex justify-content-center align-items-center exp-content'>
+                                <FontAwesomeIcon icon={faBookAtlas} className='exp-icons'/>
+                                <div>
+                                    <h3>Earn A Certificates</h3>
+                                    <p>Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
+                                </div>
+
+                            </div>
+                            <div className='d-flex justify-content-center align-items-center exp-content'>
+                                <FontAwesomeIcon icon={faBookAtlas} className='exp-icons'/>
+                                <div>
+                                    <h3>Learn with Expert</h3>
+                                    <p>Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
+                                </div>
+
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </>
     );
 };
 
